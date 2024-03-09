b4babd46820f8deba7f60ae02388367f0857396d
ZephyrusFrost <141444827+ZephyrusFrost@users.noreply.github.com>
2024-03-09 13:53:26 +0800

    Update welcome.js

--- scripts/events/welcome.js
+++ scripts/events/welcome.js
@@ -22,14 +22,20 @@ module.exports = {
 			defaultWelcomeMessage: "Xin chào {userName}.\nChào mừng bạn đến với {boxName}.\nChúc bạn có buổi {session} vui vẻ!"
 		},
 		en: {
-			session1: "morning",
-			session2: "noon",
-			session3: "afternoon",
-			session4: "evening",
+			session1: "𝗺𝗼𝗿𝗻𝗶𝗻𝗴",
+			session2: "𝗻𝗼𝗼𝗻",
+			session3: "𝗮𝗳𝘁𝗲𝗿𝗻𝗼𝗼𝗻",
+			session4: "𝗲𝘃𝗲𝗻𝗶𝗻𝗴",
 			welcomeMessage: "🟢 𝗬𝗔𝗡𝗭𝗨 𝗕𝗢𝗧  are connected successfully!\n\n━━━━━━━━━━━━━━\n🎉greetings: Thank you for inviting me to the group😊\n\n𝗕𝗢𝗧 𝗽𝗿𝗲𝗳𝗶𝘅: %1\nTo view the list of commands, please enter: %1help\n𝗔𝗗𝗠𝗜𝗡:https://www.facebook.com/profile.php?id=100052395031835\n━━━━━━━━━━━━━━\nThanks for using my Bot",
 			multiple1: "𝘆𝗼𝘂",
 			multiple2: "𝘆𝗼𝘂 𝗴𝘂𝘆𝘀",
-			defaultWelcomeMessage: `𝗛𝗲𝗹𝗹𝗼  𝗻𝗶𝗴𝗴𝗮 @{userName}.\n\n𝘄𝗲𝗹𝗰𝗼𝗺𝗲 {multiple} 𝘁𝗼 𝘁𝗵𝗲 𝗰𝗵𝗮𝘁 𝗴𝗿𝗼𝘂𝗽🎉\n𝗚𝗿𝗼𝘂𝗽 𝗻𝗮𝗺𝗲:『{boxName}』\n𝗛𝗮𝘃𝗲 𝗮 𝗻𝗶𝗰𝗲 {session} 🤖\n━━━━━━━━━━━━━━\n𝗔𝗗𝗠𝗜𝗡 𝗖𝗢𝗡𝗧𝗔𝗖𝗧:https://www.facebook.com/profile.php?id=100052395031835\n━━━━━━━━━━━━━━`
+			defaultWelcomeMessage: `𝗛𝗲𝗹𝗹𝗼  𝗻𝗶𝗴𝗴𝗮 @{userName}.\n\n𝘄𝗲𝗹𝗰𝗼𝗺𝗲 {multiple} 𝘁𝗼 𝘁𝗵𝗲 𝗰𝗵𝗮𝘁 𝗴𝗿𝗼𝘂𝗽🎉\n𝗚𝗿𝗼𝘂𝗽 𝗻𝗮𝗺𝗲:『{boxName}』\n𝗛𝗮𝘃𝗲 𝗮 𝗻𝗶𝗰𝗲 {session} 🤖\n𝗡𝗢𝗧𝗜𝗖𝗘❗❗\n⚠️ 𝗚𝗿𝗼𝘂𝗽 𝗥𝘂𝗹𝗲𝘀
+
+𝗡𝗼 𝗦𝗽𝗮𝗺𝗺𝗶𝗻𝗴: Please refrain from excessive posting or sending repeated messages. Respect others' space in the group.
+
+𝗕𝗲 𝗥𝗲𝘀𝗽𝗲𝗰𝘁𝗳𝘂𝗹: Treat everyone with kindness and consideration. Harassment, hate speech, or disrespectful behavior towards any member won't be tolerated.
+
+If you have some questions or you want to create a bot like this just PM me on my Facebook account.\n\n━━━━━━━━━━━━━━\n𝗔𝗗𝗠𝗜𝗡 𝗖𝗢𝗡𝗧𝗔𝗖𝗧:https://www.facebook.com/profile.php?id=100052395031835\n━━━━━━━━━━━━━━`
 		}
 	},
 
